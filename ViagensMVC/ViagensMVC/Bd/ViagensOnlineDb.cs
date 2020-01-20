69ab25e2c192f9151fa993c740177c4d5213d1f6
andersontmj <47406214+andersontmj@users.noreply.github.com>
2020-01-20 14:42:37 -0300

    Continuacao Destinos

---
+++ ViagensMVC/ViagensMVC/Bd/ViagensOnlineDb.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using ViagensMVC.Models;
+
+namespace ViagensMVC.Bd
+{
+    public class ViagensOnlineDb: DbContext
+    {
+        private const string conexao = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=c:\Users\di3910\Documents\GitHub\Aplication-ASP.Net\ViagensMVC\ViagensMVC\App_Data\ViagensOnlineDb.mdf;Integrated Security=True";
+
+        public ViagensOnlineDb() : base(conexao)
+        {
+
+        }
+
+        public DbSet<Destino> Destino { get; private set; }
+        public object Destinos { get; internal set; }
+    }
+}
